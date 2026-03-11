efb34d33a92176e66234e37e80993298652d51eb
kavin raj <sikavinraj@gmail.com>
2026-03-11 11:59:18 +0530

    fix: update header position to sticky and add text-transform to product card

--- website/src/components/BuildPrinciples/BuildPrinciples.js
+++ website/src/components/BuildPrinciples/BuildPrinciples.js
@@ -135,7 +135,7 @@ const principles = [
       </svg>
     ),
     title: "Continuous Evolution",
-    description: <>Always Improving, Never<br />Settling</>,
+    description: "Always Improving, Never Settling",
   },
 ];
 
